f05a51e6e3571de654951d1efa4adfb7ca963991
Alexander <al.x.falconer@gmail.com>
2021-01-24 13:22:38 -0500

    commit tree 8d4dc4449774f0c1860a28e6696627b8fb792a90

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Alex"
+  end
+
+  get '/hometown' do
+    "My hometown is Brooklyn, New York"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Green Grass' by Tom Waits"
+  end
 end
